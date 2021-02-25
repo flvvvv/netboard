e519d336a8426d7c77470b2932ec41385d7cfda9
Flvvvv <71684104+flvvvv@users.noreply.github.com>
2021-02-25 23:08:20 +0800

    Update massage.py

--- view/massage.py
+++ view/massage.py
@@ -1,8 +1,8 @@
 from flask import Blueprint
 
-from models.message.models import Message
-from models.board.models import Board
-from models.user.models import User
+from models.models import Message
+from models.models import Board
+from models.models import User
 
 from form import MessageForm
 from flask import render_template, request, redirect, url_for
